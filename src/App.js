fec635c8e1895851717512f2cf10a568915fae37
renatogsouza <renatomacedo1995@gmail.com>
2021-04-28 11:22:06 -0300

    rederizando componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import Todo from './components/Todo';
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
 
-function App() {
+
+
+function App(props) {
+  
+  function addTask(name) {
+    alert(name);
+  }
+  const taskList = props.tasks.map(task => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form  addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+      //  role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
