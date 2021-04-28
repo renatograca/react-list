6310a29e76f52575f87d5ddbe24c49479a1b7857
renatogsouza <renatomacedo1995@gmail.com>
2021-04-28 14:33:18 -0300

    ajuste form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 function Form(props) {
 
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
   function handleSubmit(e) {
     e.preventDefault();
-    props.addTask('Say Hello');
+    props.addTask(name);
+    setName("");
   }
-  
+
   return (
     <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
@@ -22,6 +28,8 @@ function Form(props) {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          value={name}
+          onChange={handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
